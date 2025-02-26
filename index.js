f31690a497e2e23326759d0beb24e4aa08cdaee9
Kr2Ka1 <kriste.kore.flora@gmail.com>
2025-02-26 13:03:08 +0200

    33 paskaita

--- index.js
+++ index.js
@@ -7,7 +7,10 @@
 //http://www.npmjs.com/package/@faker-js/faker
 //http://fakerjs.dev/api/
 const { faker } = require('@faker-js/faker');
+const { log } = require('console');
 const fs = require('fs');
+const readline = require('readline');
+
 
 //Sugeneruoti netikrą vardą ir avardę
 
@@ -78,23 +81,51 @@ function generateCompany() {
     return {
         'title': faker.commerce.department()
     }
-}
+};
 
 
-const users = Array.from({ length: 100 }, generateUser);
-const product = Array.from({ length: 100 }, generateProduct);
-const category = Array.from({ length: 100 }, generateCategory);
-const toy = Array.from({ length: 100 }, generateToy);
-const company = Array.from({ length: 100 }, generateCompany);
+function jsonToCsv(jsonArray) {
+    const csvHeader = Object.keys(jsonArray[0]).join(',') + '\n';
+
+    let csvRows = '';
+    for (let i = 0; i < jsonArray.length; i++) {
+        let csvRow = Object.values(jsonArray[i]).join(',') + '\n';
+        csvRows += csvRow;
+    }
+    return csvHeader + csvRows;
+};// primityvi ir labai paprasta ir gali greitai sulūžti
+
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Kiek duomenų nrėtumėte suvesti į kiekvieną iš duomenų failų? \n', (dataCount) => {
+    console.log(dataCount);
+
+
+
+
+
+//duomne7 kiekviename faile keitimas su kitamuoju
+
+
+const users = Array.from({ length: dataCount }, generateUser);
+const product = Array.from({ length: dataCount }, generateProduct);
+const category = Array.from({ length: dataCount }, generateCategory);
+const toy = Array.from({ length: dataCount }, generateToy);
+const company = Array.from({ length: dataCount }, generateCompany);
 console.log(users);
 console.log(users.length);
 
 //users objektą pasiversti į paprastą tekstą
-fs.writeFileSync('users.json', JSON.stringify(users, null, 4));
-fs.writeFileSync('product.json', JSON.stringify(product, null, 4));
-fs.writeFileSync('category.json', JSON.stringify(category, null, 4));
-fs.writeFileSync('toy.json', JSON.stringify(toy, null, 4));
-fs.writeFileSync('company.json', JSON.stringify(company, null, 4));
+fs.writeFileSync('json/users.json', JSON.stringify(users, null, 4));
+fs.writeFileSync('json/product.json', JSON.stringify(product, null, 4));
+fs.writeFileSync('json/category.json', JSON.stringify(category, null, 4));
+fs.writeFileSync('json/toy.json', JSON.stringify(toy, null, 4));
+fs.writeFileSync('json/company.json', JSON.stringify(company, null, 4));
 
 //pasiimam bet kokį sugenruotą vartotoją
 //JSON
@@ -113,17 +144,23 @@ fs.writeFileSync('company.json', JSON.stringify(company, null, 4));
 //Dakota, Kihn, (734) 849-4654 x0547, Kenyatta_Gutkowski@gmail.com, Tiara.Marks97, 7L_1BcUS2Pwd07c, 2025-02-25T08:12:51.215Z
 
 const csvHeader = Object.keys(users[0]).join(',') + '\n';
-fs.writeFileSync('users.csv', csvHeader);
+
 let csvRows = '';
 for (let i = 0; i < users.length; i++) {
     let csvRow = Object.values(users[i]).join(',') + '\n';
     csvRows += csvRow;
-}
+};
 
 //irašau csvHeader ir vieną csvRow
-fs.writeFileSync('users.csv', csvHeader + csvRows);
+fs.writeFileSync('csv/users.csv', jsonToCsv(users));
+fs.writeFileSync('csv/product.csv', jsonToCsv(product));
+fs.writeFileSync('csv/category.csv', jsonToCsv(category));
+fs.writeFileSync('csv/toy.csv', jsonToCsv(toy));
+fs.writeFileSync('csv/company.csv', jsonToCsv(company));
 
-console.log(csvHeader);
+// console.log(csvHeader);
+console.log('Failai sugeneruoti su '+ dataCount + ' įrašų');
+
 
 
 
@@ -143,3 +180,6 @@ console.log(csvHeader);
 //     users.push(user);
 // }
 // console.log(users);
+
+rl.close();
+});
